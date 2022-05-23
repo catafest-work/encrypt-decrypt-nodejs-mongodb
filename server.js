8bae30d6d965e0c743076acb91d20a3a5dfeb379
catalin.festila <catafest76@gmail.com>
2022-05-23 11:32:08 +0300

    decrypted data and update database ...

--- server.js
+++ server.js
@@ -21,6 +21,10 @@ const key = 'catafest-encryption-key-tutorial';
 //random 16 digit initialization vector
 const iv = crypto.randomBytes(16);
 
+// set templating engine as EJS
+app.set("view engine", "ejs");
+
+
 //start the server instance
 http.listen(process.env.PORT || 3000, function() {
   console.log("Server started reunning ...");
@@ -30,27 +34,77 @@ http.listen(process.env.PORT || 3000, function() {
         console.error("Error connecting to MongoDB")
       }
       // set database connection
-      db = client.db("encrypt_decrypt_scring");
+      db = client.db("encrypt_decrypt_string");
       console.log("Database connected");
-      // route to encrypt the message 
-      app.get("/encrypt/:message", async function(request, result) {
-        // get message from URL 
+
+      
+      // route to decrypt the message
+      app.get("/decrypt/:encrypted", async function (request, result) {
+        // get encrypted text from URL
+        const encrypted = request.params.encrypted;
+
+        // check if text exists in database
+        const obj = await db.collection("strings").findOne({
+            encryptedData: encrypted
+        });
+
+        if (obj == null) {
+            result.status(404).send("Not found");
+            return;
+        }
+
+        // convert initialize vector from base64 to buffer
+        const origionalData = Buffer.from(obj.iv, 'base64') 
+
+        // decrypt the string using encryption algorithm and private key
+        const decipher = crypto.createDecipheriv(algorithm, key, origionalData);
+        let decryptedData = decipher.update(obj.encryptedData, "hex", "utf-8");
+        decryptedData += decipher.final("utf8");
+
+        // display the decrypted string
+        result.send(decryptedData);
+      });
+
+      // route to show all encrypted messages
+      app.get("/", async function (request, result) {
+        // get all data from database
+        const data = await db.collection("strings")
+            .find({})
+            .sort({
+                _id: -1
+            }).toArray();
+
+        // render index.ejs
+        result.render("index", {
+            data: data
+        });
+      });
+
+      // route to encrypt the message
+      app.get("/encrypt/:message", async function (request, result) {
+        // get message from URL
         const message = request.params.message;
-        // encrypt the string using encyption algorithm , private key and initialization vector
+
+        // random 16 digit initialization vector
+        const iv = crypto.randomBytes(16);
+
+        // encrypt the string using encryption algorithm, private key and initialization vector
         const cipher = crypto.createCipheriv(algorithm, key, iv);
-        let encryptedData = cipher.update(message, "utf-8", "hex")
+        let encryptedData = cipher.update(message, "utf-8", "hex");
         encryptedData += cipher.final("hex");
 
-        // convert the initialization vector to base64 string 
+        // convert the initialization vector to base64 string
         const base64data = Buffer.from(iv, 'binary').toString('base64');
-        // save encrypted string along with initialization vector in database
-        await db.collection("string").insertOne({
-          iv: base64data,
-          encryptedData: encryptedData
+        
+        // save encrypted string along wtih initialization vector in database
+        await db.collection("strings").insertOne({
+            iv: base64data,
+            encryptedData: encryptedData
         });
-        // show the encrypted message in the browser 
+
+        // show the encrypted message
         result.send(encryptedData);
-      })
+      });
 
     })
 
