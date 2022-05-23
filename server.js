ba2e7f06ff6b045868b13b7f0049134ad4371dd6
catalin.festila <catafest76@gmail.com>
2022-05-23 10:45:07 +0300

    initialize the project

--- server.js
+++ server.js
@@ -32,7 +32,25 @@ http.listen(process.env.PORT || 3000, function() {
       // set database connection
       db = client.db("encrypt_decrypt_scring");
       console.log("Database connected");
-      // [route goes here]
+      // route to encrypt the message 
+      app.get("/encrypt/:message", async function(request, result) {
+        // get message from URL 
+        const message = request.params.message;
+        // encrypt the string using encyption algorithm , private key and initialization vector
+        const cipher = crypto.createCipheriv(algorithm, key, iv);
+        let encryptedData = cipher.update(message, "utf-8", "hex")
+        encryptedData += cipher.final("hex");
+
+        // convert the initialization vector to base64 string 
+        const base64data = Buffer.from(iv, 'binary').toString('base64');
+        // save encrypted string along with initialization vector in database
+        await db.collection("string").insertOne({
+          iv: base64data,
+          encryptedData: encryptedData
+        });
+        // show the encrypted message in the browser 
+        result.send(encryptedData);
+      })
 
     })
 
